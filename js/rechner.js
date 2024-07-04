7c4ca7ab96b07147d0a9eef2e75877c0064166d0
4lb10n <sektelan@gmail.com>
2024-07-04 13:36:30 +0200

    Update rechner.js

--- js/rechner.js
+++ js/rechner.js
@@ -1,7 +1,23 @@
-function kohlenhydratInverpackung(gramm) {
-    	gramm * kohlenhydrate;
-}
+var gramm = document.getElementById('gramm').value;
+var kohlenhydrate = document.getElementById('kohlenhydrate').value;
+var grammidv = document.getElementById('grammidv').value;
+var zucker = document.getElementById('zucker').value;
 
-function addierenDeszucker() {
-    
+document.getElementById("berechnen").addEventListener("click", function(){
+kohlenhydratInverpackung();
+})
+function kohlenhydratInverpackung() {
+    const ergebnis =  gramm / kohlenhydrate;
+if (ergebnis >= 4){
+ 
+	document.getElementById('result1').innerText = 'Das Ergebnis ist: Gut' + ergebnis;
+	return ergebnis;
+}  else if(ergebnis==3){
+    document.getElementById('result2').innerText = 'Das Ergebnis ist: MIttel' + ergebnis;
+    return ergebnis;
+}
+	else {
+	document.getElementById('result3').innerText = 'Das Ergebnis ist: Schlecht' + ergebnis;
+    return ergebnis;
+}
 }
